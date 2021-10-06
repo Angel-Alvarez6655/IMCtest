ab35f0f3cdbae3fabafb6dbd498a9d4e818156be
Angel-Alvarez6655 <64872369+Angel-Alvarez6655@users.noreply.github.com>
2021-10-05 19:27:15 -0700

    Add files via upload

---
+++ Test/Persona.php
@@ -0,0 +1,33 @@
+<?php
+
+Class Persona{
+    public $nombre;
+    public $peso;
+    public $estatura;
+    public $stringGenerico;
+
+    public function calcularIMC($nombre, $peso, $estatura, $stringGenerico){
+        if($estatura!=null&&$peso!=null){
+            $IMC = $peso/($estatura**2);
+            echo '<h1>CALCULADORA DE IMC</h1><br>';
+            if($IMC < 18.5){
+                echo '<h2>IMC: '.$IMC.'</h2><br>';
+                echo 'Felicidades '.$nombre.$stringGenerico.'Delgadez o bajo peso.<br>';
+            }
+            elseif($IMC >= 18.5 && $IMC <= 24.9){
+                echo '<h2>IMC: '.$IMC.'</h2><br>';
+                echo 'Enhorabuena '.$nombre.$stringGenerico.'Normales o peso saludable.<br>';
+            }
+            else{
+                echo '<h2>IMC: '.$IMC.'</h2><br>';
+                echo 'Carnal bajale a las tortillas😱 '.$nombre.$stringGenerico.'Obesidad.<br>';
+            }
+            
+        }
+        else{
+            echo 'Inserte algun valor.';
+        }
+    }
+}
+
+?>
---
+++ Test/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once('Persona.php');
+$Ramirez1 = new Persona();
+$Ramirez1 -> nombre = 'Jorgito Firulais Ramirez Perez Martinez';
+$Ramirez1 -> peso = 89;
+$Ramirez1 -> estatura = 1.5;
+
+$Ramirez1 -> calcularIMC('Jorgito Firulais Ramirez Perez Martinez',199,1.5,'. Su peso corresponde al conjunto: ');
+
+
+?>
